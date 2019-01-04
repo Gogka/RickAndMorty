a9ba7ae47a44afe4a6f0ecae224f099713faadc7
Igor Tomilin <it.goryan@mail.ru>
2019-01-04 09:30:35 +0300

    Added shadow and corner radius to cell

--- Rick&Morty/Rick&Morty/Scopes/Rick&MortyList/RMCharacterCell/RMCharacterCell.swift
+++ Rick&Morty/Rick&Morty/Scopes/Rick&MortyList/RMCharacterCell/RMCharacterCell.swift
@@ -12,8 +12,17 @@ class RMCharacterCell: UICollectionViewCell {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var blurEffectView: UIVisualEffectView!
     @IBOutlet weak var nameLabel: UILabel!
-    
     var characterId: Int?
+    
+    private let shadow: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 4
+        layer.shadowOpacity = 0.8
+        layer.fillColor = UIColor.clear.cgColor
+        return layer
+    }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,6 +32,23 @@ class RMCharacterCell: UICollectionViewCell {
         blurEffectView?.effect = UIBlurEffect(style: .dark)
         avatarImageView?.contentMode = .scaleAspectFill
         avatarImageView?.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        contentView.layer.cornerRadius = 8
+        contentView.clipsToBounds = true
+        layer.insertSublayer(shadow, below: contentView.layer)
+        clipsToBounds = false
     }
 
+//    override func didMoveToSuperview() {
+//        super.didMoveToSuperview()
+//        shadow.removeFromSuperlayer()
+//        guard let superview = self.superview else { return }
+//        superview.layer.insertSublayer(shadow, below: layer)
+//    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+        shadow.path = path
+        shadow.shadowPath = path
+    }
 }
